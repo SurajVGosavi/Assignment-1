b08958c99163823cdc7a2aaafaf26193ce60947b
Suraj <gosavisuraj53@gmail.com>
2025-07-31 12:27:14 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,17 @@
+# Task 1: Perform Basic Mathematical Operations
+
+a = int((input("Enter the first number : ")))
+b = int(input("Enter the second number : "))
+
+c = a + b
+print("Addition: ",c)
+
+c = a - b
+print("Subtraction: ",c)
+
+c = a * b
+print("Multiplication: ",c)
+
+c = a / b
+c = float(c)
+print("Division: ",c)
